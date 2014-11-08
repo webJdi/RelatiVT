3aad687d18053f2e40117fad4decc2653e091ada
Suman Senapati <sigma.6.2009@gmail.com>
2014-11-08 17:03:46 +0530

    version 1.0

--- relatiVT.js
+++ relatiVT.js
@@ -2,17 +2,31 @@ $(document).ready(function(){
 	// This is an open source project developed by Suman Senapati aka Exile101
 	// This allows you to apply simple parallax effect to your web elements
 	// Use the class relatiVT and specify the value in data-speed attrib on your HTML DOM element
-	var val=$('.relatiVT').attr("data-speed");
+/*	var val=$('.relatiVT').attr("data-speed");
 	$(window).scroll(function(){
 		var pos=$('.relatiVT').offset();
 		var scrollTop=$(window).scrollTop();
 		if((pos-120)>scrollTop)
 		{
 			$('.relatiVT').css("transform","translateY("+scrollTop/val+"px)");
-			$('.relatiVT').css("-webkit-transform","translateY("+scrollTop/val+"px)"); // for chrome
-			$('.relatiVT').css("-moz-transform","translateY("+scrollTop/val+"px)"); // for firefox
-			$('.relatiVT').css("-o-transform","translateY("+scrollTop/val+"px)"); // for opera
 		}
-	});
+	}); */
+	
+	
+	function relatiVT(el){
+		var element=$(el);
+		var dec=element.attr("data-speed");
+		$(window).scroll(function(){
+		var pos=element.offset();
+		var scrollTop=$(window).scrollTop();
+		if((pos-120)>scrollTop)
+		{
+			element.css("transform","translateY("+scrollTop/dec+"px)");
+			element.css("-webkit-transform","translateY("+scrollTop/dec+"px)");
+			element.css("-moz-transform","translateY("+scrollTop/dec+"px)");
+			element.css("-o-transform","translateY("+scrollTop/dec+"px)");
+		}
+		
+	}
 	
 });
