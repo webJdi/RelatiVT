e0dfa615b059fd604d92618cae118606aee93ff4
Suman Senapati <sigma.6.2009@gmail.com>
2014-11-09 14:57:01 +0530

    Version 1.0

--- relatiVT.js
+++ relatiVT.js
@@ -2,17 +2,8 @@ $(document).ready(function(){
 	// This is an open source project developed by Suman Senapati aka Exile101
 	// This allows you to apply simple parallax effect to your web elements
 	// Use the class relatiVT and specify the value in data-speed attrib on your HTML DOM element
-/*	var val=$('.relatiVT').attr("data-speed");
-	$(window).scroll(function(){
-		var pos=$('.relatiVT').offset();
-		var scrollTop=$(window).scrollTop();
-		if((pos-120)>scrollTop)
-		{
-			$('.relatiVT').css("transform","translateY("+scrollTop/val+"px)");
-		}
-	}); */
-	
-	
+
+		
 	function relatiVT(el){
 		var element=$(el);
 		var dec=element.attr("data-speed");
